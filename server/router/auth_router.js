62eac4dd92cd2ad2882fa9f0ee5e94cffa3642fb
21F9618 <zhalaym@gmail.com>
2024-11-25 07:23:30 +0500

    storing email

--- server/router/auth_router.js
+++ server/router/auth_router.js
@@ -1,8 +1,21 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const User = require('../models/user_models');
 
-router.route("/").get((req, res) => {
-    res.status(200).send("welcome using router");
+// Endpoint to save the user's email
+router.post('/save-email', async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            user = new User({ email });
+            await user.save();
+        }
+        res.status(200).json({ message: 'Email saved successfully', user });
+    } catch (error) {
+        res.status(500).json({ error: 'Error saving email' });
+    }
 });
 
 module.exports = router;
